fbffe75ab8de750acc7b689fd3bc8eeb5b3ec083
etech-technology <165582378+etech-technology@users.noreply.github.com>
2025-11-25 18:58:14 -0500

    Update main.tf

--- targets/dev/main.tf
+++ targets/dev/main.tf
@@ -1,4 +1,4 @@
 module "server" {
-  source = "git::https://github.com/etech-technology/infra-base-iac.git//modules/ec2?ref=main"
+  source = "git::https://github.com/etech-technology/infra-base-iac.git//modules/ec2?ref=f91a338"
  # add your configurations
 }
